c866b3be0eca770a1dc2f1b384283ada8afe29bd
ivan.pascual <ivan.pascual@basetis.com>
2020-04-07 22:10:42 +0200

    First base project commit

---
+++ Vens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vens
+//
+//  Created by Ivan Pascual Palacios on 07/04/2020.
+//  Copyright © 2020 Covid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
