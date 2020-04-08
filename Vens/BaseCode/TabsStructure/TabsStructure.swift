b6a968f447171aa0e588bef3de8e2d8547d53152
Aina Cuxart <aina.cuxart@basetis.com>
2020-04-08 20:53:49 +0200

    Change selected index and remove 4th item

--- Vens/BaseCode/TabsStructure/TabsStructure.swift
+++ Vens/BaseCode/TabsStructure/TabsStructure.swift
@@ -14,19 +14,20 @@ class TabsStructure: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpUI()
+        self.selectedIndex = getSelectedIndex()
     }
     
     func setUpUI(){
 //        let itemAppearance = UITabBarItem.appearance()
-        
+//        itemAppearance.setTitleTextAttributes([NSAttributedString.Key.font : UIFont.ccBaseFontPNSemiboldOfSize(fontSize: .textTabBar)], for: .normal)
 //        itemAppearance.setTitleTextAttributes([NSAttributedString.Key.foregroundColor : UIColor.ccWhite], for: .selected)
-        
+//
 //        let tabBarAppearance = UITabBar.appearance()
 //        tabBarAppearance.tintColor = UIColor.ccSecondaryColor
 //        tabBarAppearance.barTintColor = UIColor.ccPrimaryColor
 //        tabBarAppearance.unselectedItemTintColor = UIColor.ccWhite
         
-        guard tabBar.items != nil, tabBar.items!.count >= 4 else { return }
+        guard tabBar.items != nil, tabBar.items!.count >= 3 else { return }
         
         tabBar.items?[0].title = String(localizedStringWithKey: "tabFirstKey")
         tabBar.items?[0].image = UIImage.init(named: "alertIcon")
@@ -34,9 +35,13 @@ class TabsStructure: UITabBarController {
         tabBar.items?[1].image = UIImage.init(named: "inspectionsIcon")
         tabBar.items?[2].title = String(localizedStringWithKey: "tabThirdKey")
         tabBar.items?[2].image = UIImage.init(named: "mapIcon")
-        tabBar.items?[3].title = String(localizedStringWithKey: "tabFourthKey")
-        tabBar.items?[3].image = UIImage.init(named: "settingsIcon")
+        
     }
     
     
+    func getSelectedIndex() -> Int {
+        
+        //if hasFavourites return 0
+        return 1
+    }
 }
